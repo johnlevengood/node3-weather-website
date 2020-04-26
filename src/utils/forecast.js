1a7a19c2a8136d8aaeb777dbbb29a4b61a205a93
johnlevengood <50719877+johnlevengood@users.noreply.github.com>
2020-04-26 14:09:34 -0400

    updated forcast string

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latX, lonY, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees outside. It feels like ' + body.current.feelslike + ' degrees out.');
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees outside. It feels like ' + body.current.feelslike + ' degrees out. Humity is at ' + body.current.humidity + '%.');
         }
     })
 }
